dd6b81889d4caa3e6c9021f3972c9b8290bd0951
c0653616 <c0653616@SB-N208-15.lambton.on.ca>
2015-07-16 16:07:38 -0400

    to select the products

--- AssignmentNo-5/src/java/assignment5/ProductServlet.java
+++ AssignmentNo-5/src/java/assignment5/ProductServlet.java
@@ -6,10 +6,22 @@
 
 package assignment5;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
 /**
  *
  * @author c0653616
  */
+
 public class ProductServlet {
-    
+     @GET
+    @Produces("application/json")
+    public String doGet() {
+        return getResults("SELECT * FROM product");
+    }
+
+    private String getResults(String select__from_product) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
